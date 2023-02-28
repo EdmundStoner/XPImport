1d191ba55d411b789ad8da338f9759c4a91efd78
Edmund <113002475+EdmundStoner@users.noreply.github.com>
2023-02-28 15:05:22 -0600

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,66 @@
+#---------------------------------------------------------------------------
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#---------------------------------------------------------------------------
+
+
+import bpy
+from bpy.types import Operator
+from bpy.props import FloatVectorProperty
+from bpy_extras.object_utils import AddObjectHelper, object_data_add
+from mathutils import Vector
+
+bl_info = {
+    "name" : "XPImport",
+    "author" : "Edmund Stoner",
+    "description" : "Imports an X-Plane obj file into blender",
+    "blender" : (2, 80, 0),
+    "version" : (0, 5, 1),
+    "location" : "3DView",
+    "warning" : "",
+    "category" : "Import-Export"
+}
+
+if "" not in locals():
+
+    from . import XP_import
+else:
+    import importlib
+    XP_import = importlib.reload(XP_import)
+
+
+def menu_func(self, context):
+    self.layout.operator(XP_import.OBJECT_OT_xplane_import.bl_idname, text="XPlane Object (.obj)")
+
+
+def register():
+    bpy.utils.register_class(XP_import.OBJECT_OT_xplane_import)
+    bpy.types.TOPBAR_MT_file_import.append(menu_func)
+
+
+def unregister():
+    bpy.utils.unregister_class(XP_import.OBJECT_OT_xplane_import)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func)
+
+
+if __name__ == "__main__":
+    register()
